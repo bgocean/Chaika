3cfd027dfdc3a46f8cdc70154d21f221cfe67271
bgocean <bigocean700@gmail.com>
2024-06-17 19:14:41 +0300

    add 480 mobil menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
-  $(".header__menu, .footer__menu").on("click", "a", function (event) {
-    event.preventDefault();
-    let id = $(this).attr("href"),
-      top = $(id).offset().top;
-    $("body,html").animate({ scrollTop: top }, 1500);
-  });
+  $(".header__menu, .header__menu-mobil, .footer__menu").on(
+    "click",
+    "a",
+    function (event) {
+      event.preventDefault();
+      let id = $(this).attr("href"),
+        top = $(id).offset().top;
+      $("body,html").animate({ scrollTop: top }, 1500);
+    }
+  );
 });
