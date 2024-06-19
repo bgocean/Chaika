4a1b9e6cd0fd784f80de7c1650c4dd8de616b246
bgocean <bigocean700@gmail.com>
2024-06-19 13:44:39 +0300

    add begin form validation

--- js/main.js
+++ js/main.js
@@ -16,5 +16,40 @@ const menuList = document.querySelector(".header__menu-mobil");
 const menuMobil = document.querySelector(".header__menu-mobil");
 
 menuBtn.addEventListener("click", () => {
-  menuList.classList.toggle("menu__list--open");  
+  menuList.classList.toggle("menu__list--open");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+
+    let error = formValidate(form);
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll("._req");
+
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+
+      if (input.classList.contains("_email")) {
+      }
+    }
+    function formAddError(input) {
+      input.parentElement.classList.add("_error");
+    }
+
+    function formRemoveError(input) {
+      input.parentElement.classList.remove("_error");
+      input.classList.remove("_error");
+    }
+
+    function emailTest(input) {
+      return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+  }
 });
