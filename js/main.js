7f3b00f200ba2c330eef978c4511cd2c2f15a8c0
bgocean <bigocean700@gmail.com>
2024-06-19 21:20:42 +0300

    add modal

--- js/main.js
+++ js/main.js
@@ -21,6 +21,42 @@ menuBtn.addEventListener("click", () => {
   menuList.classList.toggle("menu__list--open");
 });
 
+// Модалка
+document.addEventListener("DOMContentLoaded", function () {
+  // Получаем модальное окно
+  var modal = document.getElementById("modal");
+
+  // Получаем кнопку, которая открывает модальное окно
+  var btn = document.getElementById("openModal");
+
+  // Получаем элемент <span>, который закрывает модальное окно
+  var span = document.getElementsByClassName("close")[0];
+
+  // Когда пользователь нажимает на кнопку, открываем модальное окно 
+  btn.onclick = function () {
+    var iframe = document.getElementById("documentFrame");
+    iframe.src = "./files/ДОГОВІР-ОФЕРТА.pdf"; // Убедитесь, что путь к файлу правильный
+    modal.style.display = "block";
+  }
+
+  // Когда пользователь нажимает на <span> (x), закрываем модальное окно
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // Когда пользователь нажимает в любом месте вне модального окна, закрываем его
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+});
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("form");
   form.addEventListener("submit", formSend);
