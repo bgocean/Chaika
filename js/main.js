5b947f184239a82b475cfff8529708feeac5695e
bgocean <bigocean700@gmail.com>
2024-06-19 23:24:20 +0300

    add modal

--- js/main.js
+++ js/main.js
@@ -23,29 +23,22 @@ menuBtn.addEventListener("click", () => {
 
 // Модалка
 document.addEventListener("DOMContentLoaded", function () {
-  // Получаем модальное окно
-  const modal = document.getElementById("modal");
-
-  // Получаем кнопку, которая открывает модальное окно
-  const btn = document.getElementById("openModal");
-
-  // Получаем элемент <span>, который закрывает модальное окно
+  
+  const modal = document.getElementById("modal");  
+  const btn = document.getElementById("openModal");  
   const span = document.getElementsByClassName("close")[0];
-
-  // Когда пользователь нажимает на кнопку, открываем модальное окно 
+  
   btn.onclick = function () {
     var iframe = document.getElementById("documentFrame");
-    iframe.src = "./files/ДОГОВІР-ОФЕРТА.pdf"; // Убедитесь, что путь к файлу правильный
+    iframe.src = "./files/ДОГОВІР-ОФЕРТА.pdf"; 
     modal.style.display = "block";
   }
-
-  // Когда пользователь нажимает на <span> (x), закрываем модальное окно
+  
   span.onclick = function () {
     modal.style.display = "none";
   }
 
-  // Когда пользователь нажимает в любом месте вне модального окна, закрываем его
-  window.onclick = function (event) {
+   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
